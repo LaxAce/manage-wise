9d271aa01ec6f771ce3321b52576be59fc87050a
Abdulhafeez <user@abdulhafeez-MacBook-Pro.local>
2024-02-17 23:54:06 +0100

    UI | Implement taskboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,133 +1,137 @@
+"use client";
+
+import useGeneralStore from "@store/general";
 import { PageLayout } from "@components/layouts";
 
 export default function Home() {
+  const isSideBarOpen = useGeneralStore(state => state.isSideBarOpen);
 
   return (
     <PageLayout>
-      <main className="pt-5 pl-5 overflow-auto h-[85svh] mr-5 sm:w-[63svw] md:w-[79svw]">
+      <main className={`pt-6 pl-5 overflow-auto h-[calc(100svh-65px)] sm:h-[calc(100svh-81px)] md:h-[calc(100svh-89px)] ${isSideBarOpen ? "sm:w-[calc(100svw-250px)] md:w-[calc(100svw-300px)]" : "w-full"}`}>
         <div className="flex gap-6 w-svw">
-          <div className="w-[280px]">
+          <div className="w-[280px] flex-shrink-0">
             <span className="flex items-center mb-6 gap-2 text-xs text-gray-828FA3 tracking-[2.4px] font-semibold"><span className="bg-[#49C4E5] w-[15px] h-[15px] rounded-full" /> TODO (4)</span>
 
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Research pricing points of various competitors and trial different business models</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Research pricing points of various competitors and trial different business models</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
           </div>
-          <div className="w-[280px]">
+          <div className="w-[280px] flex-shrink-0">
             <span className="flex items-center mb-6 gap-2 text-xs text-gray-828FA3 tracking-[2.4px] font-semibold"><span className="bg-[#8471F2] w-[15px] h-[15px] rounded-full" /> DOING (6)</span>
 
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Add account management endpoints</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Add account management endpoints</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Research pricing points of various competitors and trial different business models</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Research pricing points of various competitors and trial different business models</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
           </div>
-          <div className="w-[280px]">
+          <div className="w-[280px] flex-shrink-0">
             <span className="flex items-center mb-6 gap-2 text-xs text-gray-828FA3 tracking-[2.4px] font-semibold"><span className="bg-[#67E2AE] w-[15px] h-[15px] rounded-full" /> DONE (4)</span>
 
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Research pricing points of various competitors and trial different business models</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Research pricing points of various competitors and trial different business models</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
-            </div>
-          </div>
-          <div className="w-[280px]">
-            <span className="flex items-center mb-6 gap-2 text-xs text-gray-828FA3 tracking-[2.4px] font-semibold"><span className="bg-[#49C4E5] w-[15px] h-[15px] rounded-full" /> TODO (4)</span>
-
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
-            </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
-            </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
-            </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Research pricing points of various competitors and trial different business models</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
-            </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
           </div>
-          <div className="w-[280px]">
+          <div className="w-[280px] flex-shrink-0">
             <span className="flex items-center mb-6 gap-2 text-xs text-gray-828FA3 tracking-[2.4px] font-semibold"><span className="bg-[#49C4E5] w-[15px] h-[15px] rounded-full" /> TODO (4)</span>
 
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Research pricing points of various competitors and trial different business models</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Research pricing points of various competitors and trial different business models</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
-            <div className="py-6 mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
-              <h6 className="font-semibold">Build UI for onboarding flow</h6>
-              <p className="text-xs text-gray-828FA3 font-semibold">0 of 6 substasks</p>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
+            </div>
+          </div>
+          <div className="w-[280px] flex-shrink-0">
+            <span className="flex items-center mb-6 gap-2 text-xs text-gray-828FA3 tracking-[2.4px] font-semibold"><span className="bg-[#49C4E5] w-[15px] h-[15px] rounded-full" /> TODO (4)</span>
+
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
+            </div>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
+            </div>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
+            </div>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Research pricing points of various competitors and trial different business models</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
+            </div>
+            <div className="py-[23px] mb-5 bg-white-FFFFFF px-4 rounded-lg shadowx shadow-gray-E4EBFA shadow-lg">
+              <h6 className="text-[15px] leading-[19px]">Build UI for onboarding flow</h6>
+              <p className="text-xs text-gray-828FA3 mt-2">0 of 6 substasks</p>
             </div>
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,31 +9,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
-    },
-    colors: {
-      violet: {
-        "635FC7": "#635FC7",
-        "A8A4FF": "#A8A4FF",
-      },
-      black: {
-        "000112": "#000112",
-        "20212C": "#20212C",
-      },
-      gray: {
-        "2B2C37": "#2B2C37",
-        "3E3F4E": "#3E3F4E",
-        "828FA3": "#828FA3",
-        "E4EBFA": "#E4EBFA",
-      },
-      white: {
-        "F4F7FD": "#F4F7FD",
-        "FFFFFF": "#FFFFFF",
-      },
-      red: {
-        "EA5555": "#EA5555",
-        "FF9898": "#FF9898",
+      colors: {
+        violet: {
+          "635FC7": "#635FC7",
+          "A8A4FF": "#A8A4FF",
+        },
+        black: {
+          "000112": "#000112",
+          "20212C": "#20212C",
+        },
+        gray: {
+          "2B2C37": "#2B2C37",
+          "3E3F4E": "#3E3F4E",
+          "828FA3": "#828FA3",
+          "E4EBFA": "#E4EBFA",
+        },
+        white: {
+          "F4F7FD": "#F4F7FD",
+          "FFFFFF": "#FFFFFF",
+        },
+        red: {
+          "EA5555": "#EA5555",
+          "FF9898": "#FF9898",
+        }
       }
-    },
+    }
   },
   plugins: [],
 };
