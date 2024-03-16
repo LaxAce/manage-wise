e032a124c9a5e1f6291d82c122ea740cb94be707
Abdulhafeez <laxhameedhafeez@gmail.com>
2024-03-16 23:35:32 +0100

    ft/implement add new task modal (WIP)

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -1,5 +1,4 @@
 import { InputProps } from "@components/common/types";
-import { Close } from "@icons";
 
 const Input = ({ label, id, type = "text", value, onChange, placeholder, className, rows = 5, icon }: InputProps) => {
     return (
@@ -24,9 +23,9 @@ const Input = ({ label, id, type = "text", value, onChange, placeholder, classNa
                         placeholder={placeholder}
                         className={`w-full px-[17px] py-2 bg-inherit border border-gray-828FA340 rounded text-[13px] font-medium leading-[23px] placeholder:text-black-000112 placeholder:dark:text-white-FFFFFF placeholder:opacity-25 placeholder:text-[13px] placeholder:leading-[23px] placeholder:font-medium focus-visible:outline-none ${className}`}
                     />
-                    <div className="cursor-pointer">
+                    {icon && (<div className="cursor-pointer">
                         {icon}
-                    </div>
+                    </div>)}
                 </div>)}
 
 
